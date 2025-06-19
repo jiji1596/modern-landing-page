59a56569015c63c85274a48b497c696419bd36c1
Shunji Sugaya <sugayasc33@gmail.com>
2025-06-19 21:48:59 +0900

    changed readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-This website was built with the help of a tutorial from this YouTube video.
+This website was built with the help of a tutorial from this YouTube video https://www.youtube.com/watch?v=b4i8l73DU6A .
 I translated the original TypeScript (TSX) version into JavaScript (JSX).
--- src/components/shared/Container.jsx
+++ src/components/shared/Container.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 export const Container = ({children, className}) => {
-  return <div className={`mx-auto max-w-7xl w-full px-5 sm:px-8 md:px-14 lg:px-5`}></div>
+  return <div className={`mx-auto max-w-7xl w-full px-5 sm:px-8 md:px-14 lg:px-5 ${className}`}>{children}</div>
 
 }
 
